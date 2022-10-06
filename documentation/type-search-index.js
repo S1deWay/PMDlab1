733760f8745e30c67370c26ffb4db83455b77a61
S1deWay <eliseev1999@inbox.ru>
2022-10-06 08:32:54 +0700

    Add documentation

---
+++ documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.pmd_lab1","l":"ExampleInstrumentedTest"},{"p":"com.example.pmd_lab1","l":"ExampleUnitTest"},{"p":"com.example.pmd_lab1","l":"MainActivity"}]
